7aab8c42279f38fcb9653a1f4ddbdf770c22de0d
Colerar <233hbj@gmail.com>
2023-04-08 18:20:43 +0800

    feat: improve parsing args

--- include/labyrinth/Registry.h
+++ include/labyrinth/Registry.h
@@ -2,11 +2,58 @@
 #define LABYRINTH_REGISTRY_H
 
 #include "labyrinth_export.h"
+#include "llvm/ADT/StringRef.h"
+#include "llvm/Passes/PassBuilder.h"
 
-#include <span>
+#include <functional>
 
 namespace labyrinth {
-void passBuilderCallback(llvm::PassBuilder &builder);
+void passBuilderCallback(llvm::PassBuilder &args);
+
+bool parseArgs(
+    llvm::StringRef args, llvm::StringRef name,
+    const std::function<bool(llvm::StringRef k, llvm::StringRef v)> &parser =
+        [](auto, auto) { return false; });
 } // namespace labyrinth
+
+enum class OnEmpty {
+  True,
+  False,
+  Fail,
+};
+
+/**
+ * @param val which reference the result will be assigned to
+ * @return Returns true if parsing is successful, false otherwise
+ */
+template <OnEmpty on_empty = OnEmpty::Fail>
+bool parseBoolValue(llvm::StringRef key, llvm::StringRef value, bool &val) {
+  if (value.empty()) {
+    if constexpr (on_empty == OnEmpty::True) {
+      val = true;
+      return true;
+    } else if constexpr (on_empty == OnEmpty::False) {
+      val = false;
+      return true;
+    } else {
+      llvm::errs() << "Invalid empty value for key `" << key << "`\n";
+      return false;
+    }
+  }
+
+  if (value.equals_insensitive("on") || value.equals_insensitive("true") ||
+      value.equals_insensitive("1")) {
+    val = true;
+  } else if (value.equals_insensitive("off") ||
+             value.equals_insensitive("false") ||
+             value.equals_insensitive("0")) {
+    val = false;
+  } else {
+    llvm::errs() << "Invalid value for `" << key << "`: " << value << '\n';
+    return false;
+  }
+
+  return true;
+}
 
 #endif // LABYRINTH_REGISTRY_H
--- src/Registry.cpp
+++ src/Registry.cpp
@@ -1,56 +1,71 @@
-#include <llvm/ADT/StringRef.h>
-#include <llvm/Passes/PassBuilder.h>
-#include <llvm/Passes/PassPlugin.h>
-
-#include "labyrinth/GlobalsEncryption.h"
 #include "labyrinth/Registry.h"
+#include "labyrinth/GlobalsEncryption.h"
+#include "llvm/Passes/PassPlugin.h"
 
 using labyrinth::GlobalsEncryptionPass;
 using llvm::ModulePassManager, llvm::OptimizationLevel;
 using llvm::outs, llvm::errs;
 using llvm::StringRef;
 
-void labyrinth::passBuilderCallback(llvm::PassBuilder &builder) {
-  builder.registerPipelineParsingCallback(
-      [](StringRef name, ModulePassManager &manager, auto opt_level) {
-        llvm::SmallVector<StringRef, 8> split;
-        name.split(split, ":", -1, false);
-        if (split.empty()) {
-          errs() << "Pass name is empty\n";
+namespace labyrinth {
+
+void passBuilderCallback(llvm::PassBuilder &builder) {
+  builder.registerPipelineParsingCallback([](StringRef args,
+                                             ModulePassManager &manager, auto) {
+    bool only_str = false;
+    uint8_t obf_time = 1;
+    bool parse_status = parseArgs(args, "gle", [&](StringRef k, StringRef v) {
+      if (k.equals_insensitive("onlystr")) {
+        if (!parseBoolValue<OnEmpty::True>(k, v, only_str)) {
           return false;
         }
-        if (split.front() != "gle") {
+      } else if (k.equals_insensitive("times")) {
+        if (!to_integer(v, obf_time, 10)) {
+          errs() << "Invalid value for `" << k << "`: " << v << '\n';
           return false;
         }
-        bool only_str = false;
-        uint32_t obf_time = 1;
-        for (auto entry : llvm::make_range(split.begin() + 1, split.end())) {
-          auto [k, v] = entry.split("=");
-          if (k.equals_insensitive("only-str") ||
-              k.equals_insensitive("onlystr")) {
-            if (v.empty() || v.equals_insensitive("on") ||
-                v.equals_insensitive("true")) {
-              only_str = true;
-            } else if (v.equals_insensitive("off") ||
-                       v.equals_insensitive("false")) {
-              only_str = false;
-            } else {
-              errs() << "Invalid value for `" << k << "`: " << v << '\n';
-              return false;
-            }
-          } else if (k.equals_insensitive("times") ||
-                     k.equals_insensitive("obftime") ||
-                     k.equals_insensitive("obf-time")) {
-            if (!to_integer(v, obf_time, 10)) {
-              errs() << "Invalid value for `" << k << "`: " << v << '\n';
-              return false;
-            }
-          }
-        }
-        manager.addPass(GlobalsEncryptionPass(only_str, obf_time));
-        return true;
-      });
+      } else {
+        errs() << "Unknown configuration key `" << k << "`\n";
+        return false;
+      }
+      return true;
+    });
+    if (!parse_status) {
+      return false;
+    }
+
+    manager.addPass(GlobalsEncryptionPass(only_str, obf_time));
+    return true;
+  });
 }
+
+bool parseArgs(
+    StringRef args, StringRef name,
+    const std::function<bool(llvm::StringRef k, llvm::StringRef v)> &parser) {
+  llvm::SmallVector<StringRef, 8> split;
+  args.split(split, ":", -1, false);
+  if (split.empty()) {
+    errs() << "Pass name is empty\n";
+    return false;
+  }
+  if (split.front() != name) {
+    return false;
+  }
+  return std::ranges::all_of(split.begin() + 1, split.end(), [&](auto entry) {
+    auto [k, v] = entry.split("=");
+    if (k.empty()) {
+      errs() << "A parameter for pass `" << name << "` is empty\n";
+      return false;
+    }
+    if (!parser(k, v)) {
+      errs() << "Invalid configuration entry: `" << entry << "`\n";
+      return false;
+    }
+    return true;
+  });
+}
+
+} // namespace labyrinth
 
 extern "C" llvm::PassPluginLibraryInfo LABYRINTH_EXPORT
 llvmGetPassPluginInfo() {
