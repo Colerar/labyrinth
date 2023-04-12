6b6daccc66f2ffc8252f37d665bc47011df4aac5
Colerar <233hbj@gmail.com>
2023-04-12 20:03:54 +0800

    fix(fla): make default width = 64, warning if width > 64

--- src/Registry.cpp
+++ src/Registry.cpp
@@ -38,25 +38,32 @@ void passBuilderCallback(llvm::PassBuilder &builder) {
     manager.addPass(GlobalsEncryptionPass(only_str, obf_time));
     return true;
   });
-  builder.registerPipelineParsingCallback(
-      [](StringRef args, ModulePassManager &manager, auto) {
-        uint32_t width = 128;
-        bool status = parseArgs(args, "fla", [&](StringRef k, StringRef v) {
-          if (k.equals_insensitive("width")) {
-            if (!to_integer(v, width, 10) || width < 32 || width > 512) {
-              errs() << "Invalid value for `" << k << "`: " << v << '\n';
-              return false;
-            }
-          }
-          return true;
-        });
-        if (!status) {
+  builder.registerPipelineParsingCallback([](StringRef args,
+                                             ModulePassManager &manager, auto) {
+    uint32_t width = 64;
+    bool status = parseArgs(args, "fla", [&](StringRef k, StringRef v) {
+      if (k.equals_insensitive("width")) {
+        if (!to_integer(v, width, 10) || width < 32 || width > 512) {
+          errs() << "Invalid value for `" << k << "`: " << v << '\n';
           return false;
         }
+      }
+      return true;
+    });
+    if (!status) {
+      return false;
+    }
 
-        manager.addPass(FlatteningPass(width));
-        return true;
-      });
+    if (width > 64) {
+      outs()
+          << "WARNING[fla]: bit-width is greater than 64, which could "
+             "potentially corrupt the code logic during following LLVM passes. "
+             "Please take care to address this issue.";
+    }
+
+    manager.addPass(FlatteningPass(width));
+    return true;
+  });
 }
 
 bool parseArgs(
